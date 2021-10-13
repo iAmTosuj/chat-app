88714f0f81fa828140abd80497b05418d2df71be
Tosuj <sanches00799@gmail.com>
2021-10-13 20:03:24 +0300

    feat: add controller to bloc

--- lib/bloc/chat_page_bloc.dart
+++ lib/bloc/chat_page_bloc.dart
@@ -1,15 +1,22 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 part 'chat_page_event.dart';
 part 'chat_page_state.dart';
 
 class ChatPageBloc extends Bloc<ChatPageEvent, ChatPageState> {
-  ChatPageBloc() : super(ChatPageInitial()) {
-    on<ChatPageEvent>((event, emit) {
-      // TODO: implement event handler
+  final ItemScrollController itemScrollController;
+  final ItemPositionsListener itemPositionsListener;
+
+  ChatPageBloc(
+      {required this.itemScrollController, required this.itemPositionsListener})
+      : super(ChatPageState()) {
+    itemPositionsListener.itemPositions.addListener(() => print(
+        itemPositionsListener.itemPositions.value.map((val) => val.index)));
+
+    on<ChatPageJump>((event, emit) {
+      itemScrollController.jumpTo(index: event.index);
     });
   }
 }
--- lib/bloc/chat_page_state.dart
+++ lib/bloc/chat_page_state.dart
@@ -1,6 +1,14 @@
 part of 'chat_page_bloc.dart';
 
-@immutable
-abstract class ChatPageState {}
+class ChatPageState {
+  final int indexBottomElem;
+  final int indexToScroll;
 
-class ChatPageInitial extends ChatPageState {}
+  ChatPageState({this.indexBottomElem = 0, this.indexToScroll = 0});
+
+  ChatPageState copyWith({int? indexBottomElem, int? indexToScroll}) {
+    return ChatPageState(
+        indexBottomElem: indexBottomElem ?? this.indexBottomElem,
+        indexToScroll: indexToScroll ?? this.indexToScroll);
+  }
+}
