fe67b07e1b9c2429c9e219fd569a6097fc5b5df1
Tosuj <sanches00799@gmail.com>
2021-10-13 19:35:28 +0300

    feat: add library for scroll control

--- lib/src/chat_page/chat_page.dart
+++ lib/src/chat_page/chat_page.dart
@@ -1,45 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+
+  ChatPage({Key? key}) : super(key: key) {
+    itemPositionsListener.itemPositions.addListener(() => print(
+        itemPositionsListener.itemPositions.value.map((val) => val.index)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: [
+            Expanded(
+                child: ScrollablePositionedList.builder(
+              itemCount: 30,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  Container(
+                      height: 60,
+                      width: double.infinity,
+                      child: Text('Item $index')),
+                  Divider()
+                ],
+              ),
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
+            )),
             Align(
               alignment: Alignment.bottomCenter,
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.document_scanner),
-                    splashRadius: 24,
-                  ),
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          fillColor: const Color(0xFFF7F7F7),
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: const BorderSide(
-                              width: 0,
-                              style: BorderStyle.none,
-                            ),
-                          ),
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Сообщение...",
-                          contentPadding: const EdgeInsets.all(8)),
-                      minLines: 1,
-                      maxLines: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.document_scanner),
+                      splashRadius: 24,
                     ),
-                  ))
-                ],
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            fillColor: const Color(0xFFF7F7F7),
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            hintText: "Сообщение...",
+                            contentPadding: const EdgeInsets.all(8)),
+                        minLines: 1,
+                        maxLines: 5,
+                      ),
+                    )),
+                    IconButton(
+                      onPressed: () {
+                        itemScrollController.jumpTo(index: 20);
+                      },
+                      icon: const Icon(
+                        Icons.document_scanner,
+                        color: Colors.amber,
+                      ),
+                      splashRadius: 24,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
